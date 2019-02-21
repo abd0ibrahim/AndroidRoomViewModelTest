dd494965ea304f58b4abfb26aaec107eddcd63af
Abdelrahman Ibrahim <iiabdo22@gmail.com>
2019-02-21 14:19:06 +0200

    -add entity to Word Model

--- app/src/main/java/com/myfirstapplicationexample/smartec/myapplication/Word.java
+++ app/src/main/java/com/myfirstapplicationexample/smartec/myapplication/Word.java
@@ -1,9 +1,11 @@
 package com.myfirstapplicationexample.smartec.myapplication;
 
 import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+@Entity(tableName = "word_table")
 public class Word {
 
     @PrimaryKey
--- app/src/main/java/com/myfirstapplicationexample/smartec/myapplication/WordViewModel.java
+++ app/src/main/java/com/myfirstapplicationexample/smartec/myapplication/WordViewModel.java
@@ -1,22 +1,24 @@
 package com.myfirstapplicationexample.smartec.myapplication;
 
 import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
 
 import java.util.List;
 
-public class WordViewModel extends ViewModel {
+public class WordViewModel extends AndroidViewModel {
 
     private WordRepository mRepository;
     private LiveData<List<Word>> mAllWords;
 
     public WordViewModel(Application application) {
+        super(application);
         this.mRepository = new WordRepository(application);
         this.mAllWords = mRepository.getAllWords();
     }
 
-    LiveData<List<Word>> getAllWords() {
+    public LiveData<List<Word>> getAllWords() {
         return mAllWords;
     }
 
